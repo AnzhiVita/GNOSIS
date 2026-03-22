c7981a145a6a10d3084e604324b34ae507ed2f09
AnzhiVita <126701606+AnzhiVita@users.noreply.github.com>
2026-03-22 16:54:02 +0500

    Создала файл config.py с кодом, добавила в код 2 ссылки на разные книги

---
+++ Phase_1/config.py
@@ -0,0 +1,7 @@
+# config.py
+SOURCES = [
+    {"type": "rss", "url": "https://lib.ru/NTL/KIBERNETIKA/IWANOW_W/odd_even.txt"},
+    {"type": "rss", "url": "https://lib.ru/POECHIN/TZAOCHI/tzao_lyrics.txt"},
+    # можно добавить API погоды, новости науки и т.п.
+]
+SLEEP_INTERVAL = 600  # секунд
